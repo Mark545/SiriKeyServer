faed4c77e6b30a8b391a0fcc182220ca338fd785
Sumer Kohli <sumer.112@gmail.com>
2011-12-30 12:52:14 -0800

    Add New Features To Install Script

--- install.sh
+++ install.sh
@@ -3,8 +3,14 @@
 echo "Welcome To This Script Made By Firebolt55439 On Github. "
 echo "Downloading Required Programs... "
 apt-get install build-essential dnsmasq ruby rubygems ruby-dev openssl
-echo "Configuring DnsMasq... "
+echo "Configuring Ruby and Ruby Gems..."
+echo "Downloading Required Ruby Gems..."
+gem install eventmachine
+gem install CFPropertyList
+gem install uuidtools
+echo "Configuring DNS Server... "
 cp conf/dnsmasq.conf /etc/dnsmasq.conf
+echo "Restarting And Applying Changes To DNS..."
 /etc/init.d/dnsmasq restart
 echo "Generating Certificates... "
 sudo ./certificate_generator.sh #Certificate Generator
